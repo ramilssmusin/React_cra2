4d0c712bd4c22e65ddd3e13a7486562068d8dbe3
ramilssmusin <QAZwsx1@#*3579>
2020-02-22 12:50:59 -0400

    Updates

---
+++ precache-manifest.f64c0a8fd3a336e60ff8b47442b32d16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ec0712ba5d269e8ca39fd688a1cd77",
+    "url": "/React_cra2/index.html"
+  },
+  {
+    "revision": "2111b1dbee69100befc7",
+    "url": "/React_cra2/static/css/main.ca264e0d.chunk.css"
+  },
+  {
+    "revision": "a311fe074a71650b1932",
+    "url": "/React_cra2/static/js/2.65295476.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_cra2/static/js/2.65295476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2111b1dbee69100befc7",
+    "url": "/React_cra2/static/js/main.643e8eae.chunk.js"
+  },
+  {
+    "revision": "f18041f749628b17cd56",
+    "url": "/React_cra2/static/js/runtime-main.19afffd5.js"
+  }
+]);
